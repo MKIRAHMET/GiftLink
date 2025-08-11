31d4fbd9ce717d6f7b516c8d932682ce751016a1
Muhammed <muhammed@192.168.2.13>
2025-08-11 18:31:14 +0300

    search api bug fixes

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -5,12 +5,11 @@ const connectToDatabase = require('../models/db');
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
+
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
 
-        // Initialize the query object
+
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
@@ -18,24 +17,19 @@ router.get('/', async (req, res, next) => {
             query.name = { $regex: req.query.name.trim(), $options: "i" };
         }
 
-        // Task 3: Add other filters to the query
-
-        // Filter by category (exact match)
-        if (req.query.category && req.query.category.trim() !== '') {
-            query.category = req.query.category.trim();
+        if (req.query.category) {
+            query.category = req.query.category;
         }
 
         // Filter by condition (exact match)
-        if (req.query.condition && req.query.condition.trim() !== '') {
-            query.condition = req.query.condition.trim();
+        if (req.query.condition) {
+            query.condition = req.query.condition;
         }
 
-        if (req.query.age_years && !isNaN(req.query.age_years) && parseInt(req.query.age_years) >= 0) { 
+        if (req.query.age_years) { 
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
         const gifts = await collection.find(query).toArray();
         res.json(gifts);
     } catch (e) {
