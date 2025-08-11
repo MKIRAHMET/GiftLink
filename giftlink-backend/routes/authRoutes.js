5eeb6e55be569a83b136888c1a7f1ad7f2bd5cbe
Muhammed <muhammed@Users-MacBook-Pro.local>
2025-08-12 01:48:03 +0300

    register

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -6,9 +6,9 @@ const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
-const pino = require('pino');  // Import Pino logger
+const pino = require('pino'); 
 
-const logger = pino();  // Create a Pino logger instance
+const logger = pino(); 
 
 dotenv.config();
 
@@ -16,20 +16,18 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+
         const db = await connectToDatabase();
 
-        // Task 2: Access MongoDB collection
         const collection = db.collection("users");
 
-        //Task 3: Check for existing email
+
         const existingEmail = await collection.findOne({ email: req.body.email });
 
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
 
-        //Task 4: Save user details in database
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
